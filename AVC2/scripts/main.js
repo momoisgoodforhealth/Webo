3bf8009a5660d59e5702812045105c99b143e0ca
momoisgoodforhealth <shoban.shakya@gmail.com>
2022-03-08 09:14:15 -0500

    Testing Fizzbuzz function

--- AVC2/scripts/main.js
+++ AVC2/scripts/main.js
@@ -1,3 +1,39 @@
 let fizzbuzz = 'Fizzbuzz';
 let fizz = 'Fizz';
 let buzz = 'Buzz';
+
+var fizzBuzz = function (n) {
+    let x = 0;
+    let bool1 = false;
+    let bool2 = false;
+    for (let i = 1; i <= n; i++) {
+        bool1 = false;
+        bool2 = false;
+        let balls1 = i % 3;
+        let balls2 = i % 5;
+
+        if (balls1 == 0) {
+            bool1 = true;
+        }
+
+        if (balls2 == 0) {
+            bool2 = true;
+        }
+
+        if (bool1 == true && bool2 == true) {
+            x = fizzbuzz;
+        }
+        else if (bool1 == true) {
+            x = fizz;
+        }
+        else if (bool2 == true) {
+            x = buzz;
+        }
+        else {
+            x = String(i);
+        }
+    }
+    return x;
+};
+const myHeading = document.querySelector('h1');
+myHeading.textContent = fizzBuzz(2);
