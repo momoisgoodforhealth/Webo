0db0f91db126f0a784ee8f27e7d6967c9af8e64a
momoisgoodforhealth <shoban.shakya@gmail.com>
2022-03-09 09:48:56 -0500

    Retrieve from API test

--- AVC2/scripts/main.js
+++ AVC2/scripts/main.js
@@ -1,6 +1,7 @@
 let fizzbuzz = 'Fizzbuzz';
 let fizz = 'Fizz';
 let buzz = 'Buzz';
+let url = 'http://basic-web.dev.avc.web.usf.edu/:id';
 
 let fizzBuzz = function (n) {
     let x = 0;
@@ -38,14 +39,96 @@ let fizzBuzz = function (n) {
 
 let value = 0;
 
+function get(url) {
+    return new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+        http.onload = function () {
+            resolve({ status: http.status, data: JSON.parse(http.response) });
+        };
+        http.open("GET", url);
+        http.send();
+    });
+}
+
+function post(url, data) {
+    data = JSON.stringify(data);
+    return new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest();
+        http.onload = function () {
+            resolve({ status: http.status, data: JSON.parse(http.response) });
+        };
+        http.open("POST", url);
+        //Make sure that the server knows we're sending it json data.
+        http.setRequestHeader("Content-Type", "application/json");
+        http.send(data);
+    });
+}  
+
 
 window.addEventListener('load', () => {
     const myHeading = document.getElementById('user-name');
     myHeading.textContent = localStorage.getItem('username');
 })
+const responseheading = document.getElementById('result');
+
+get(url).then(function (response) {
+
+    //Put all code that relies on the data from this request in here.
+
+    if (response.status == 200) {
+        const username = response.data.id; //The username that was requested. In this case it is "myUserName".
+        const score = response.data.score; //The user's current score.
+        responseheading.textContent = username;
+
+    }
+    else {
+        //User "myUserName" not found.
+        //response.data is null
+        post("https://example.com/myUserName", { score: 0 }); //create a new user.
+    }
+});
+
+/*var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'http://basic-web.dev.avc.web.usf.edu/')
+ourRequest.onload = function () {
+    var ourData = JSON.parse(ourRequest.responseText);
+    responseheading.textContent = ourData
+    console.log('yolo');
+};  */
+
 const myHeading2 = document.querySelector('h2');
 const button = document.getElementById('button');
 button.onclick = function () {
     value++;
     myHeading2.textContent = fizzBuzz(value);
+    let userscore = { username: localStorage.getItem('username'), score: value };
+   /*   post(url, userscore).then(function (response) {
+    switch (response.status) {
+      case 200:
+       //User was updated successfully.
+        //response.data will be the same as returned by get(), and should contain the updated data.
+            console.log(200)
+        const score = response.data.score;
+        break;
+      case 201:
+        //A new user was successfully created. Otherwise same as status 200.
+        console.log(200)
+         score = response.data.score;
+        break;
+      case 400:
+        //Bad request. Most likely your data that you sent (in this case dataToSend) was formatted incorrectly, or you supplied a negative score value.
+        //response.data will be: { Error: "error message" }
+  console.error(response.data);
+        break;
+      case 500:
+       //Something went wrong on the server, such as the database got deleted somehow. This should never happen.
+        //response.data will be the same as status 400.
+  console.error(response.data);
+        break;
+    }
+          });   */
 }
+
+
+
+
